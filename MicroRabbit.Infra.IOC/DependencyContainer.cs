f988c3b435de2d68f6b070b725442cfb63268d81
mohammedheiba <mohammedheiba@yahoo.com>
2023-03-08 16:45:38 +0200

    initial commit of infrastructure dependency project

---
+++ MicroRabbit.Infra.IOC/DependencyContainer.cs
@@ -0,0 +1,15 @@
+﻿using MicroRabbit.Domain.Core.Bus;
+using MicroRabbit.Infra.Bus;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MicroRabbit.Infra.IOC
+{
+    public class DependencyContainer
+    {
+        public static void RegisterServices(IServiceCollection services)
+        {
+            //Domain bus
+            services.AddTransient<IEventBus, RabbitMQBus>();
+        }
+    }
+}
